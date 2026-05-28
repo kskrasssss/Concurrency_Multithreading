a7e2faec679234da92c5190934990ac2b6f6f69b
ks.krasssss <self.developmentkkras@gmail.com>
2026-05-28 06:17:22 +0300

    Create project with concurrency includes

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <thread>      // std::thread Ч створенн€ поток≥в
+#include <mutex>       // std::mutex Ч захист в≥д data race
+#include <condition_variable>  // std::condition_variable Ч синхрон≥зац≥€
+#include <queue>       // std::queue Ч черга дл€ producer-consumer
+#include <string>
+
+int main() {
+    std::cin.get();
+    return 0;
+}
